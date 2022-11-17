024840f501ef94a3dfe0aafb8220d4a536a18d63
João Souza <tygasouzaa@gmail.com>
2022-11-17 02:53:26 -0300

    initial commit

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,10 @@
+import {Button} from '@ignite-ui/react'
+
+import {StoryObj, Meta} from '@storybook/react'
+
+export default {
+    title: "Button",
+    component: Button
+} as Meta
+
+export const Default : StoryObj =  {} 
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$gray900',
+  borderRadius: '$md',
+  width: '$full'
+})
+//for use styled components with with run
+//npx sb init --builder @storybook/builder-vite --type react --use-npm  
+ 
